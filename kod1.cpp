73a8eb1bbac5eaf7a6da33e1b0bdf5caa79baacd
Netre21 <37020207+Netre21@users.noreply.github.com>
2018-03-03 19:07:39 +0100

    v0.1

---
+++ kod1.cpp
@@ -0,0 +1,25 @@
+#include "dodaw.h"
+#include<iostream>
+using namespace std;
+int main()
+{
+Statystyki Paladyn;
+Paladyn.pocz();
+for(int i=0;i<8;i++)
+    {
+    Paladyn.czytaj();
+    }
+for(int i=0;i<5;i++)
+    {
+    Paladyn.trenuj();
+    }
+for(int i=0;i<10;i++)
+    {
+    Paladyn.studiuj();
+    }
+for(int i=0;i<12;i++)
+    {
+    Paladyn.bron();
+    }
+Paladyn.podglad();
+}
